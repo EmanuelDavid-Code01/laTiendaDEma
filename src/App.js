910e3482107f9309ec0acbeac6d29736f7b8995e
Emanuel <emadg07.ed@gmail.com>
2023-03-14 15:15:35 -0300

    agregamos npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion para obtener componentes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Iniciamos react EMA escribiendo en el App.js en una etiqueta parrafo sin "errores al momento"
         </p>
         <a
           className="App-link"
