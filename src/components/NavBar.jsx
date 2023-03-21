7ec7f2d55f78638b9ffc2f7abff87a8aa76a1717
Emanuel <emadg07.ed@gmail.com>
2023-03-21 15:25:52 -0300

    Rutas terminadas a la pagina de Home y Ofertas pero con error de link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,10 +24,10 @@ const NavBar = () => {
       </Menu>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="/ofertas">Ofertas</a>
         </li>
         <li className="nav-item">
           <a className="nav-link disabled">Disabled</a>
